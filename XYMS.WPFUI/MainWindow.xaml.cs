26d50944d1dd1c19b162838c373fb5667f396388
陈明旭 <cloomcmx1554@hotmail.com>
2018-11-13 19:59:11 +0800

    1

--- XYMS.WPFUI/MainWindow.xaml.cs
+++ XYMS.WPFUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using XYMS.WPFControlLib.About;
 using XYMS.WPFControlLib.DataBaseSetting;
 
 namespace XYMS.WPFUI
@@ -14,12 +15,20 @@ namespace XYMS.WPFUI
         {
             InitializeComponent();
         }
-
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SwitchAboutControlCommand_Executed(null, null);
+        }
         #region 命令实现
         private void OpenDataBaseSettingWindowCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             var dataBaseSettingWindow = new DataBaseSettingWindow();
             dataBaseSettingWindow.ShowDialog();
+        }
+        private void SwitchAboutControlCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var control = new AboutControl();
+            LoadUserControl(control);
         }
         #endregion
         #region 私有方法
--- XYMS.WPFUI/MainWindowCommands.cs
+++ XYMS.WPFUI/MainWindowCommands.cs
@@ -9,6 +9,10 @@ namespace XYMS.WPFUI
         /// </summary>
         public static RoutedUICommand OpenDataBaseSettingWindow { get; }
         /// <summary>
+        /// 切换关于控件
+        /// </summary>
+        public static RoutedUICommand SwitchAboutControl { get; }
+        /// <summary>
         /// 构造方法
         /// </summary>
         static MainWindowCommands()
@@ -16,6 +20,10 @@ namespace XYMS.WPFUI
             OpenDataBaseSettingWindow = new RoutedUICommand(
                 "数据库配置",
                 nameof(OpenDataBaseSettingWindow),
+                typeof(MainWindowCommands));
+            SwitchAboutControl = new RoutedUICommand(
+                "关于",
+                nameof(SwitchAboutControl),
                 typeof(MainWindowCommands));
         }
     }
